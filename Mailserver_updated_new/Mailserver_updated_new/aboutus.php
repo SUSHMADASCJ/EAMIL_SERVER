8a431750248e6b5e6e80ee0dfbf3d7d64a107dd6
SUSHMADASCJ <51118772+SUSHMADASCJ@users.noreply.github.com>
2019-12-03 08:54:55 +0530

    Add files via upload

---
+++ Mailserver_updated_new/Mailserver_updated_new/aboutus.php
@@ -0,0 +1,7 @@
+<h2 style="background:#99FF33;padding:5px" align="left">About us</h2>
+<p>Electronic mail (email or e-mail) is a method of exchanging messages ("mail") between people using electronic devices</p>
+<p>Here we have built this project using PHP and MYSQL.</p>
+<p>
+Any user who registers to this site are able to send a mail, receive a mail, draft it and can delete it</p>
+<p>
+BUILT BY SUSHMA C.J. AND THANUSHREE .S. </p>  
---
+++ Mailserver_updated_new/Mailserver_updated_new/contactus.php
@@ -0,0 +1,34 @@
+<style>
+	table{padding:5px;border-radius:5px}
+	td{padding:10px}
+</style>
+<table width="90%" border="1" align="center">
+  <tr style="background:#CCCCCC" height="30px">
+    <th colspan="2" style="color:#0000FF">Contact Us</th>
+  </tr>
+  <tr>
+    <td width="121" height="37">Name</td>
+    <td width="252">SUSHMA.C.J.</td>
+	<td width="252">THANUSHREE.S.</td>
+  </tr>
+  <tr>
+    <td height="36">Mobile </td>
+    <td> 9341971956 </td>
+	<td> 7406313223 </td>
+  </tr>
+  <tr>
+    <td>Address</td>
+    <td>Bangalore karnataka-560091</td>
+	<td>Bangalore karnataka-560091</td>
+  </tr>
+    <tr>
+    <td>Email</td>
+    <td>sushmadascj@gmail.com </td>
+	<td>tanu.shree7640@gmail.com </td>
+  </tr>
+   <tr>
+    <td>Website</td>
+    <td><a href="https://sushmacj.wixsite.com/cjtsbprojects">WEBSITE</a></td>
+  </tr>
+  
+</table>
